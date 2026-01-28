659581dda55aeda61cd38b3e5321d046cd1b46a9
Lmayagoitia <lmayagoitia@students.stonehill.edu>
2026-01-28 18:25:02 -0500

    Lab 2 Commit 1
    testing

---
+++ lab2-git/Lab_2.R
@@ -0,0 +1,7 @@
+# ----- Loading and Exploring the iris dataset -----
+
+# Load the dataset -- note that this is a built-in dataset in R
+data(iris)
+
+# View the first few rows to understand its structure
+head(iris)
